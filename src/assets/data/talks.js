a001de76b93f5ba9f9dc10fa852529e3c68b7ec4
alosori <alosori@bancolombia.com.co>
2022-11-14 00:12:55 -0500

    add talk resources

--- src/assets/data/talks.js
+++ src/assets/data/talks.js
@@ -6,7 +6,11 @@ module.exports = [
     resources: [
       {
         name: 'Slides [ES]',
-        url: 'https://www.meetup.com/es/aws-medellin/events/288978341/'
+        url: 'https://github.com/aws-medellin/data-streaming-aws-iot-redshift/raw/1223491bb2314afcad0134c7fff32e35689cc113/AWS%20UG%20Medellin%20-%20Nov%202022.pptx'
+      },
+      {
+        name: 'Code Repo',
+        url: 'https://github.com/aws-medellin/data-streaming-aws-iot-redshift/'
       },
       {
         name: 'MeetUp [ES]',
